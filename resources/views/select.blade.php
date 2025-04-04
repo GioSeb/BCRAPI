afabd283f63094c25830da19e69b361a32af4a7f
Sebastian <sebastiangioiose@gmail.com>
2025-04-04 16:53:29 -0300

    finished first step in select view

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -4,5 +4,7 @@
 @section('content')
     <div class="select-main">
         <div class="select-logo"><img src="{{asset('/img/logo.jpg')}}" alt=""></div>
+        <a href=""><div class="select-informe"><h1>NUEVA CONSULTA</h1></div></a>
     </div>
+
 @endsection
